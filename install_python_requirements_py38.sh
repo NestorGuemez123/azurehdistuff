9e06b800bbf3019ff707b7cef8e56c5ce28ea6d4
nguemez <nestorguemezche@gmail.com>
2020-12-01 16:20:25 -0600

    nothing

--- install_python_requirements_py38.sh
+++ install_python_requirements_py38.sh
@@ -1,1 +1,1 @@
-sudo /usr/bin/anaconda/envs/py38/bin/pip install configparser numpy scikit-learn xgboost
+sudo /usr/bin/anaconda/envs/py38/bin/pip install configparser numpy scikit-learn xgboost pyspark
