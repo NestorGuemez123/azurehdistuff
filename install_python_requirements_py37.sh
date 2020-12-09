5f902d61d022ce5d3190165b54a77d3671bb56d8
nguemez <nestorguemezche@gmail.com>
2020-12-09 12:43:26 -0600

    nothing

--- install_python_requirements_py37.sh
+++ install_python_requirements_py37.sh
@@ -1,1 +1,2 @@
-sudo /usr/bin/anaconda/envs/py37/bin/pip install configparser numpy pandas scikit-learn xgboost>=1.2,<1.3
+sudo /usr/bin/anaconda/envs/py37/bin/pip uninstall xgboost
+sudo /usr/bin/anaconda/envs/py37/bin/pip install configparser numpy pandas scikit-learn "xgboost>=1.2,<1.3"
